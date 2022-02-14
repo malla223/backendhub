fb41f4fb75d3e67a830e395dd0dfceb1ccda5631
Your Name <you@example.com>
2022-02-14 19:09:51 +0100

    adminCrud

---
+++ backendHub/src/main/java/com/odkmali/backendHub/Controllers/AdminController.java
@@ -0,0 +1,57 @@
+package com.odkmali.backendHub.Controllers;
+
+
+import com.odkmali.backendHub.Services.AdminServiceImplements;
+import com.odkmali.backendHub.enumeration.AdminEnum;
+import com.odkmali.backendHub.enumeration.Etat;
+import com.odkmali.backendHub.model.Admin;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/api/admin")
+public class AdminController {
+
+    @Autowired
+    AdminServiceImplements adminServiceImplements;
+
+    @PostMapping("/saveAdmin")
+    @ResponseBody
+    public Admin SaveAdmin(@RequestBody Admin admin){
+        return adminServiceImplements.SaveAdmin(admin);
+    }
+
+    @GetMapping("/getAllAdmin")
+    @ResponseBody
+    public List<Admin> getAllAdmin() {
+        return adminServiceImplements.getAllAdmin();
+    }
+
+    @GetMapping("/getAdminById/{id}")
+    public Admin getAdminById(@PathVariable("id") Long id) {
+        return adminServiceImplements.getAdminById(id);
+    }
+    @GetMapping("/getAdminByEtat/{etat}")
+    public List<Admin> getAdminByEtat(@PathVariable("etat") Etat etat) {
+        return adminServiceImplements.getAdminByEtat(etat);
+    }
+
+    @GetMapping("/getAdninByType/{type}")
+    public List<Admin> getAdminByType(@PathVariable("type") AdminEnum type) {
+        return adminServiceImplements.getAdminByType(type);
+    }
+
+    @DeleteMapping("/deleteAdmin/{id}")
+    public void deleteAdmin(@PathVariable("id") Long id) {
+        adminServiceImplements.deleteAdmin(id);
+    }
+
+    @GetMapping("/restaurerAdmin/{id}")
+    public void restaurerAdmin(@PathVariable("id") Long id) {
+        adminServiceImplements.restaurerAdmin(id);
+    }
+
+}
---
+++ backendHub/src/main/java/com/odkmali/backendHub/Services/AdminServiceImplements.java
@@ -0,0 +1,64 @@
+package com.odkmali.backendHub.Services;
+
+import com.odkmali.backendHub.enumeration.AdminEnum;
+import com.odkmali.backendHub.enumeration.Etat;
+import com.odkmali.backendHub.model.Admin;
+import com.odkmali.backendHub.repository.AdminRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+public class AdminServiceImplements implements AdminService{
+
+    @Autowired
+    AdminRepo adminRepo;
+
+    public Admin SaveAdmin(Admin admin){
+        Optional<Admin> optionalAdmin = adminRepo.findAdmin(admin.getLogin_admin());
+
+        if(optionalAdmin.isPresent()){
+            System.out.println("Ce login existe deja!");
+        }else{
+            adminRepo.save(admin);
+        }
+        return admin;
+    }
+
+    @Override
+    public List<Admin> getAllAdmin() {
+        return adminRepo.getAllAdmin();
+    }
+
+    @Override
+    public Admin getAdminById(Long id) {
+        return adminRepo.findById(id).get();
+    }
+
+    @Override
+    public List<Admin> getAdminByEtat(Etat etat) {
+        return adminRepo.getAdminByEtat(etat);
+    }
+
+    @Override
+    public List<Admin> getAdminByType(AdminEnum type) {
+        return adminRepo.getAdminByType(type);
+    }
+
+    @Override
+    public void deleteAdmin(Long id) {
+        adminRepo.deleteAdmin(id);
+
+    }
+
+    @Override
+    public void restaurerAdmin(Long id) {
+        adminRepo.restaurerAdmin(id);
+
+    }
+
+
+}
